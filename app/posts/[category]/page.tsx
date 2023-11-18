f3c18af07135c3b4df00d2f3bc9bdcc1f7d937c6
6cpsmidnight <cyberelectro101@gmail.com>
2023-11-17 19:52:42 -0500

    Bug fix

--- app/posts/[category]/page.tsx
+++ app/posts/[category]/page.tsx
@@ -2,25 +2,26 @@ import { Suspense } from "react";
 import { notFound } from "next/navigation";
 import { fetchArticles, fetchCategoryArticles } from "@/app/lib/articles-data";
 import ArticlesPreview from "@/app/articles-preview";
-import type { Metadata, ResolvingMetadata } from "next";
+// import type { Metadata, ResolvingMetadata } from "next";
 
-type Props = {
-  params: { category: string };
-};
+// type Props = {
+//   params: { category: string };
+// };
 
-export async function generateMetadata(
-  { params }: Props,
-  parent: ResolvingMetadata
-): Promise<Metadata> {
-  const category = params.category;
+// export async function generateMetadata(
+//   { params }: Props,
+//   parent: ResolvingMetadata
+// ): Promise<Metadata> {
+//   const category = params.category;
 
-  return {
-    title: category.charAt(0).toUpperCase() + category.slice(1),
-  };
-}
+//   return {
+//     title: category.charAt(0).toUpperCase() + category.slice(1),
+//   };
+// }
 
-export default async function Page({ params }: Props) {
-  const category = params.category;
+export default async function Page() {
+  // { params }: Props
+  // const category = params.category;
   // const catergoryArticles = await fetchCategoryArticles(category);
   const catergoryArticles = await fetchArticles();
 
