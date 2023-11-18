1946c38d056f86920b5424aef96684cf420b91ba
6cpsmidnight <cyberelectro101@gmail.com>
2023-11-17 20:11:42 -0500

    BUG FIX

--- app/page.tsx
+++ app/page.tsx
@@ -2,9 +2,14 @@ import { Suspense } from "react";
 import ArticlesPreview from "@/app/articles-preview";
 import { ArticlesPreviewSkeleton } from "@/app/skeletons";
 import { fetchArticles } from "@/app/lib/articles-data";
+import { notFound } from "next/navigation";
 
 export default async function Home() {
   const articles = await fetchArticles();
+
+  if (!articles) {
+    notFound();
+  }
 
   return (
     // <Suspense fallback={<ArticlesPreviewSkeleton />}>
--- app/posts/[category]/page.tsx
+++ app/posts/[category]/page.tsx
@@ -1,50 +1,70 @@
+// import { Suspense } from "react";
+// import { notFound } from "next/navigation";
+// import { fetchArticles, fetchCategoryArticles } from "@/app/lib/articles-data";
+// import ArticlesPreview from "@/app/articles-preview";
+// // import type { Metadata, ResolvingMetadata } from "next";
+
+// // type Props = {
+// //   params: { category: string };
+// // };
+
+// // export async function generateMetadata(
+// //   { params }: Props,
+// //   parent: ResolvingMetadata
+// // ): Promise<Metadata> {
+// //   const category = params.category;
+
+// // if (!article) {
+// //   notFound();
+// // }
+
+// //   return {
+// //     title: category.charAt(0).toUpperCase() + category.slice(1),
+// //   };
+// // }
+
+// export default async function Page() {
+//   // { params }: Props
+//   // const category = params.category;
+//   // const catergoryArticles = await fetchCategoryArticles(category);
+//   // const catergoryArticles = await fetchArticles();
+
+//   // if (!catergoryArticles) {
+//   //   notFound();
+//   // }
+
+//   // return (
+//   //   <div>
+//   //     <Suspense fallback={<div>TEST</div>}>
+//   //       <ArticlesPreview
+//   //         articles={catergoryArticles.Articles}
+//   //         ishomepage={false}
+//   //       />
+//   //     </Suspense>
+//   //   </div>
+//   // );
+
+//   const articles = await fetchArticles();
+
+//   return (
+//     // <Suspense fallback={<ArticlesPreviewSkeleton />}>
+//     <ArticlesPreview articles={articles.Articles} ishomepage={true} />
+//     // </Suspense>
+//   );
+// }
+
 import { Suspense } from "react";
-import { notFound } from "next/navigation";
-import { fetchArticles, fetchCategoryArticles } from "@/app/lib/articles-data";
 import ArticlesPreview from "@/app/articles-preview";
-// import type { Metadata, ResolvingMetadata } from "next";
+import { ArticlesPreviewSkeleton } from "@/app/skeletons";
+import { fetchArticles } from "@/app/lib/articles-data";
+import { notFound } from "next/navigation";
 
-// type Props = {
-//   params: { category: string };
-// };
-
-// export async function generateMetadata(
-//   { params }: Props,
-//   parent: ResolvingMetadata
-// ): Promise<Metadata> {
-//   const category = params.category;
-
-// if (!article) {
-//   notFound();
-// }
-
-//   return {
-//     title: category.charAt(0).toUpperCase() + category.slice(1),
-//   };
-// }
-
-export default async function Page() {
-  // { params }: Props
-  // const category = params.category;
-  // const catergoryArticles = await fetchCategoryArticles(category);
-  // const catergoryArticles = await fetchArticles();
-
-  // if (!catergoryArticles) {
-  //   notFound();
-  // }
-
-  // return (
-  //   <div>
-  //     <Suspense fallback={<div>TEST</div>}>
-  //       <ArticlesPreview
-  //         articles={catergoryArticles.Articles}
-  //         ishomepage={false}
-  //       />
-  //     </Suspense>
-  //   </div>
-  // );
-
+export default async function Home() {
   const articles = await fetchArticles();
+
+  if (!articles) {
+    notFound();
+  }
 
   return (
     // <Suspense fallback={<ArticlesPreviewSkeleton />}>
