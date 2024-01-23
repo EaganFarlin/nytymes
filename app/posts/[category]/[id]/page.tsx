5f4e31dc28c3c28e1030125a5922bd2f4b6ce7b6
6cpsmidnight <cyberelectro101@gmail.com>
2024-01-23 17:25:40 -0500

    QoL changes

--- app/posts/[category]/[id]/page.tsx
+++ app/posts/[category]/[id]/page.tsx
@@ -43,35 +43,36 @@ export default async function Page({ params }: Props) {
       <Suspense fallback={<div>TEST</div>}>
         <div className="flex justify-center w-full">
           <div className="w-full md:w-3/5">
-            <div className="text-lg">
-              <Link href={"/"} className="text-gray-500">
-                Posts
-              </Link>{" "}
-              {">"}{" "}
-              <Link href={`/posts/${category}`} className="text-gray-950">
+            <div className="mb-4 text-xl">
+              <Link href={"/"}>Posts</Link> {">"}{" "}
+              <Link href={`/posts/${category}`}>
                 {category.charAt(0).toUpperCase() + category.slice(1)}
               </Link>
             </div>
-            <h1 className="text-2xl">{article.name}</h1>
-            <p className="text-lg">{article.description}</p>
-            <div className="flex flex-col w-full h-[calc(25vw+20vh)]">
+            <h1 className="text-3xl">{article.name}</h1>
+            <p className="mb-4 text-xl text-gray-600">{article.description}</p>
+            <div className="flex flex-col w-full">
+              <Link
+                className="w-0 h-0 relative self-end top-4 right-14"
+                href={`/posts/${category}/${id}/cover-img`}
+              >
+                <FontAwesomeIcon
+                  icon={faMaximize}
+                  className="w-10 h-10 bg-white"
+                />
+              </Link>
               <img
                 src={article.coverUri}
                 alt="article-cover"
                 className="w-full h-[calc(25vw+20vh)] object-cover"
               />
-              <Link
-                className="relative self-end bottom-[calc(25vw+20vh-1rem)] right-4"
-                href={`/posts/${category}/${id}/cover-img`}
-              >
-                <FontAwesomeIcon
-                  icon={faMaximize}
-                  className="w-8 h-8 md:w-10 md:h-10 bg-white"
-                />
-              </Link>
+              <small>{article.id}</small>
             </div>
-            <small>{article.id}</small>
-            <p className="text-lg">{article.text}</p>
+            {article.textParagraphs.map((paragraph: String, index: number) => (
+              <p key={`paragraph-${index}`} className="my-8 text-xl">
+                {paragraph}
+              </p>
+            ))}
           </div>
         </div>
       </Suspense>
